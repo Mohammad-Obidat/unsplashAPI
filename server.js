871006af57ce01c6895cebd9621303d913aee0ed
mohmdSalah <mo.s.obidat@gmail.com>
2023-07-14 03:29:42 +0300

    init server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+// const api = require('./routes/api.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+// app.use('/', api);
+
+const PORT = process.env.PORT;
+app.listen(PORT, console.log(`listening on ${PORT}`));
