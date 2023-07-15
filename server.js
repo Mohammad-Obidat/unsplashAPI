b499b89bda19cdbebde8e133309b6e60667a59cd
mohmdSalah <mo.s.obidat@gmail.com>
2023-07-16 00:18:58 +0300

    removing heroku setup

--- server.js
+++ server.js
@@ -9,15 +9,5 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('API Running'));
 app.use('/', require('./routes/api/products.js'));
 
-// serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log(`listening on ${PORT}`));
