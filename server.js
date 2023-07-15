b5f2cc27a6508f8e1934723a29077708491380ca
mohmdSalah <mo.s.obidat@gmail.com>
2023-07-16 00:46:58 +0300

    The App is ready

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('API Running'));
-app.use('/', require('./routes/api/products.js'));
+app.use('/api/', require('./routes/api/products.js'));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log(`listening on ${PORT}`));
