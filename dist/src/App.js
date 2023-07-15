88fbf9e69152450def7651dc4c33fe8676b5d1ec
mohmdSalah <mo.s.obidat@gmail.com>
2023-07-15 09:14:09 +0300

    adding a linked navbar

--- dist/src/App.js
+++ dist/src/App.js
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import ImageGallery from './components/ImageGallery';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' element={<ProductList />} />
           <Route path='/imageGallery' element={<ImageGallery />} />
--- dist/src/components/navbar.jsx
+++ dist/src/components/navbar.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import { LinkContainer } from 'react-router-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function navbar() {
-  return <>navbar</>;
+  return (
+    <>
+      <Navbar
+        bg='dark'
+        data-bs-theme='dark'
+        expand='lg'
+        className='bg-body-tertiary mb-2'
+      >
+        <Container fluid>
+          <Navbar.Brand to='/'>TA'AL Technical Test</Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='me-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <LinkContainer to='/'>
+                <Nav.Link>Product List</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/imageGallery'>
+                <Nav.Link>Image Gallery</Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Form className='d-flex'>
+              <Form.Control
+                type='search'
+                placeholder='Search'
+                className='me-2'
+                aria-label='Search'
+              />
+              <Button variant='outline-success'>Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default navbar;
